ed5ae6bf0af4608ac02d91d2309f7c26a4e35124
Nathan Brickett <natey37@gmail.com>
2019-12-03 16:53:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,16 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  {
+    id: 9
+  }
 end
 
 def my_hash_creator(key, value)
